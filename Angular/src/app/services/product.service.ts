7f7853e20d97425e2b8783baf8f2cb2a4f5b2a10
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-11 15:11:59 +0700

    routes successfully

--- Angular/src/app/services/product.service.ts
+++ Angular/src/app/services/product.service.ts
@@ -13,8 +13,10 @@ export class ProductService {
 
   getProductList(theCategoryId : number):Observable<Product[]>{
 
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
     //need to build URL based on category id ... will come back to this
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     )
   }
