5a5721aa80e689b33a5ed30d5ab9bae3ba782f94
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-13 18:43:17 +0700

    19 07 add Checkout

--- Angular/src/app/components/product-list/product-list.component.ts
+++ Angular/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 
@@ -28,6 +30,7 @@ export class ProductListComponent implements OnInit {
 
 
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route : ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -123,5 +126,13 @@ export class ProductListComponent implements OnInit {
       this.thePageSize = pageSize;
       this.thePageNumber = 1;
       this.listProducts();
+    }
+
+    addToCart(theProduct: Product){
+      console.log(`Thêm vào giỏ hàng: ${theProduct.name},${theProduct.unitPrice}`)
+
+      //TODO
+      const theCartItem = new CartItem(theProduct);
+      this.cartService.addToCart(theCartItem);
     }
 }
